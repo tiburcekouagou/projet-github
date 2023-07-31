3310933bd408e2ffb4876508104821344ec00521
Rene assigname <alidassigname@gmail.com>
2023-07-31 18:48:11 +0200

    album-search by alid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { AlbumsComponent } from './albums/albums.component';
-=======
-import { AlbumDetailsComponent } from './album-details/album-details.component';
->>>>>>> ac2dd8dec99874eab1174b27e4cd4539b02928a2
-import { AlbumDetailsComponent } from './album-details/album-details.component';
+ import { AlbumDetailsComponent } from './album-details/album-details.component';
 
 
 import { AlbumsComponent } from './albums/albums.component';
 
-import { AlbumDetailsComponent } from './album-details/album-details.component';
-import { LoginComponent } from './login/login.component';
+ import { LoginComponent } from './login/login.component';
+import { AlbumSearchComponent } from './album-search/album-search.component';
  
 
- 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumSearchComponent
   ],
   imports: [
     BrowserModule
