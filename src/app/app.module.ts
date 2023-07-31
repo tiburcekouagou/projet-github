ac4d833d07d2143036dc82dc36b8c5a6eb3313d5
Rene assigname <alidassigname@gmail.com>
2023-07-31 18:41:42 +0200

    deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AlbumSearchComponent } from './album-search/album-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlbumSearchComponent
+    
   ],
   imports: [
     BrowserModule
