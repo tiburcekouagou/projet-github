0a4bb82c9fd0788af355483284eb997e5e17af3b
Rene assigname <alidassigname@gmail.com>
2023-07-31 18:05:06 +0200

    album-search create by alid

---
+++ src/app/album-search/album-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-search',
+  templateUrl: './album-search.component.html',
+  styleUrls: ['./album-search.component.css']
+})
+export class AlbumSearchComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PaginateComponent } from './paginate/paginate.component';
+import { AlbumSearchComponent } from './album-search/album-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaginateComponent
+    PaginateComponent,
+    AlbumSearchComponent
   ],
   imports: [
     BrowserModule
