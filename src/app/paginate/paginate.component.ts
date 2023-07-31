b3b3fccd7834950ef5d5369cf6d58f70bcebf66b
Ahoumenou Emile <ahoumenouemile022@gmail.com>
2023-07-31 17:21:03 +0100

    Ajout du component albums

---
+++ src/app/paginate/paginate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paginate',
+  templateUrl: './paginate.component.html',
+  styleUrls: ['./paginate.component.css']
+})
+export class PaginateComponent {
+
+}
