76316a41543b1540945660892e35af86747a19dc
regisatl <regisattolou21@outlook.fr>
2023-07-31 17:35:58 +0100

    ajout de rien

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlbumsDescriptionComponent } from './albums-description/albums-description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumsDescriptionComponent
   ],
   imports: [
     BrowserModule
