6522d4f5a6689fe1339b729e528331895f94376b
Euloge <eagnoun@gmail.com>
2023-07-31 18:16:09 +0100

    mise à jour...

---
+++ src/app/album.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlbumService {
+
+  private _albums: Album[] = ALBUMS
+  _albumsList: any;
+
+  constructor() { }
+
+  getAlbums() {
+    return this._albums.sort((a: Album, b: Album) => a.duration - b.duration)
+  };
+
+
+  getAlbum(id: string): Album | undefined {
+    return this._albums.find(album => album.id === id)
+  }
+
+  getAlbumList(id: string): List | undefined {
+    return this._albumsList.find(list => list.id === id)
+  }
+
+  count() {
+    return this._albums.length
+  }
+
+  paginate(start: number, end: number): Album[] {
+    return this.getAlbums().slice(start, end)
+   }
+}
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlbumService {
+
+  constructor() { }
+}
