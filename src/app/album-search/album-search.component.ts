c24f268f675232c047594f1b6f40485a260f065c
wilgaslove <wilfried216@gmail.com>
2023-07-31 20:01:54 +0200

    search

--- src/app/album-search/album-search.component.ts
+++ src/app/album-search/album-search.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+import { NgForm } from '@angular/forms';
+import { AlbumService } from '../album.service';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-album-search',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./album-search.component.css']
 })
 export class AlbumSearchComponent {
+word!: string;
+@Output() searchAlbums: EventEmitter<Album[]> = new EventEmitter()
+
+  constructor(
+    private albumService: AlbumService,
+  ) { }
+
+ // ngOnInit() {}
+
+  onSubmit(form: NgForm): void {
+     // récupération d'une valeur spécifique
+     let results: Album[] = this.albumService.search(form.value.word)
+     this.searchAlbums.emit(results)
+    console.log(form.value['word']);
+    }
+
+    onChangeEmite($enent: string){
+      let results: Album [] = this.albumService.search($event);
+      this.searchAlbums.emit(results)
+    }
+ 
+
+
+
 
 }
