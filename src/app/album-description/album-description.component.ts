ba822bdfdd957b89595bcb5cc61950b8fb3cbb30
Possidius Capo <possidiusc@gmail.com>
2023-08-01 20:07:14 +0200

    modification de composant de l'album-description

--- src/app/album-description/album-description.component.ts
+++ src/app/album-description/album-description.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Album } from '../album';
+import { ActivatedRoute } from '@angular/router';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-album-description',
   templateUrl: './album-description.component.html',
   styleUrls: ['./album-description.component.css']
 })
-export class AlbumDescriptionComponent {
+export class AlbumDescriptionComponent implements OnInit {
+  album: Album | undefined
+  constructor(
+    private route : ActivatedRoute,
+    private aS : AlbumService
+  ){}
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    const id = this.route.snapshot.params['albumId']
+
+    this.album = this.aS.getAlbum(id)
+    
+  }
 
 }
